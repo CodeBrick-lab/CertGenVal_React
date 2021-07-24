8300c81cb72d76bd60427c264b520364291d0fd5
CodeBrick-lab <86566795+CodeBrick-lab@users.noreply.github.com>
2021-07-24 18:52:47 +0530

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+  Name: "Coders Evoke",
+  Thumbnail: "",
+  Banner: "https://i.imgur.com/omGelcJ.png",
+  CertificateTypes: {
+    PART: "Participants",
+    MENT: "Mentors"
+  }
+};
